1c3cb8be4addbf2c83666f8b99bd32da96daba71
nathaniel <rodriguelefortn@hotmail.com>
2024-11-10 10:59:10 -0500

    Changed hierarchy, Small refactoring, Added start of input manager

--- Assets/Scripts/Core/MessageSender/IMessageReceiver.cs
+++
@@ -1,7 +0,0 @@
-namespace UMeGames.Core.MessageSender
-{
-    public interface IMessageReceiver
-    {
-        abstract void ReceiveMessage(object message, object[] data);
-    }
-}
--- Assets/Scripts/Core/Views/MainCanvas.cs
+++
@@ -1,9 +0,0 @@
-namespace UMeGames.Core.Views
-{
-    using UnityEngine;
-
-    public class MainCanvas : MonoBehaviour
-    {
-
-    }
-}
