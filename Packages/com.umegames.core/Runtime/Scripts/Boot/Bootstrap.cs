003610d42ca9cb8d6ce2af1427581641a50511f7
UMessis <rodriguelefortn@hotmail.com>
2024-12-14 13:39:13 -0500

    Make core as a package

---
+++ Packages/com.umegames.core/Runtime/Scripts/Boot/Bootstrap.cs
@@ -0,0 +1,28 @@
+namespace UMeGames.Core.Boot
+{
+    using System;
+    using System.Collections;
+    using UMeGames.Core.Records;
+    using UMeGames.Core.Services;
+    using UMeGames.Core.Views;
+    using UnityEngine;
+
+    public class Bootstrap : MonoBehaviour
+    {
+        public Action OnInitializationComplete;
+
+        void Awake()
+        {
+            StartCoroutine(Initialize());
+        }
+
+        IEnumerator Initialize()
+        {
+            RecordHub.InitializeRecords();
+            var serviceInitializer = new ServiceInitializer();
+            yield return serviceInitializer.InitializeServices();
+            ViewManager.Instance.Initialize();
+            OnInitializationComplete?.Invoke();
+        }
+    }
+}
